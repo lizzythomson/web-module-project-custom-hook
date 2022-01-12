46eb6d8264409572db6855fce1e5e4267e86d536
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2022-01-12 13:43:33 -0700

    composed useLocalStorage around useDarkMode hook

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,10 +1,7 @@
-import { useState } from 'react';
-
 import useLocalStorage from './useLocalStorage';
 
 const useDarkMode = () => {
-  const [darkMode, setDarkMode] = useState(false);
-
+  const [darkMode, setDarkMode] = useLocalStorage('coinDataDarkMode', false);
   return [darkMode, setDarkMode];
 };
 
